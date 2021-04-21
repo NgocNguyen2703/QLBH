99438599915365ef9ec388f005e60989bc647009
Nguyen Thi Ngoc <nguyenthingoc270300@gmail.com>
2021-04-21 23:49:08 +0700

    RegisterAndLogin

--- BTLLTQL/Controllers/CheckAccountController.cs
+++ BTLLTQL/Controllers/CheckAccountController.cs
@@ -10,27 +10,59 @@ namespace BTLLTQL.Controllers
 {
     public class CheckAccountController : Controller
     {
+        Encrytion encry = new Encrytion();
+        BTLDbConText db = new BTLDbConText();
         //Mặc định là get
-        public ViewResult Login(string returnUrl)
+        [HttpGet]
+        public ActionResult Login()
         {
-            ViewBag.returnUrl = returnUrl;
             return View();
         }
         //Nhận DL từ client gửi lên
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult Login(CheckAccount Checkacc, String returnUrl)
+        public ActionResult Login(CheckAccount Checkacc)
         {
-            //Nếu vượt qua được Validation ở accountmodel
-            if (Checkacc.CheckUsername == "admin" && Checkacc.CheckPassword == "123123")
+            if (ModelState.IsValid)
             {
-                //Set Cookie
-                FormsAuthentication.SetAuthCookie(Checkacc.CheckUsername, true);
-                return RedirectToLocal(returnUrl);
+                string encrytionpass = encry.PasswordEncrytion(Checkacc.CheckPassword);
+                var model = db.CheckAccounts.Where(m => m.CheckUsername == Checkacc.CheckUsername && m.CheckPassword == encrytionpass).ToList().Count();
+                //Thong tin dang nhap chinh xac
+                if (model == 1)
+                {
+                    FormsAuthentication.SetAuthCookie(Checkacc.CheckUsername, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thong tin dang nhap khong chinh xac");
+                }
             }
             return View(Checkacc);
         }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(CheckAccount Checkacc)
+        {
+            if (ModelState.IsValid)
+            {
+                //Ma hoa mat khau truoc khi luu vao database
+                Checkacc.CheckPassword = encry.PasswordEncrytion(Checkacc.CheckPassword);
+                db.CheckAccounts.Add(Checkacc);
+                db.SaveChanges();
+                return RedirectToAction("Login", "CheckAccount");
+            }
+            return View(Checkacc);
+        }
+
         //Hàm đăng xuất khỏi chương trình
         public ActionResult Logoff()
         {
---
+++ BTLLTQL/Models/Encrytion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace BTLLTQL.Models
+{
+    //Ma hoa mat khau
+    public class Encrytion
+    {
+        public string PasswordEncrytion(string pass)
+        {
+            return FormsAuthentication.HashPasswordForStoringInConfigFile(pass.Trim(), "MD5");
+        }
+    }
+}
